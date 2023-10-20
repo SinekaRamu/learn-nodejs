4ba523cc3ef2eea7a2669af61e4152e6e22b330e
SinekaRamu <sineka2001cud@gmail.com>
2023-10-20 23:16:21 +0530

    package json

--- app/index.js
+++ app/index.js
@@ -1,3 +1,10 @@
+const config = require("./config");
+
+const app = express();
+app.listen(config.port, () => {
+  console.log(`server running on port ${config.port}`);
+});
+
 //importing utils using common js
 const { stringLength } = require("./utils/stringHelper");
 console.log("hello World");
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  port: 7892,
+};
+
+//module.exports = config;
+export default config;
